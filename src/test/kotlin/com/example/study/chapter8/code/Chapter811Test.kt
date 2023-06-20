abde468c03ccadd98f4c22e863fc4ce7f5639aae
junho3 <chojunho433@gmail.com>
2023-06-20 22:23:28 +0900

    8.1 테스트 코드 추가

---
+++ src/test/kotlin/com/example/study/chapter8/code/Chapter811Test.kt
@@ -0,0 +1,45 @@
+package com.example.study.chapter8.code
+
+import com.example.study.chapter8.code.Chapter811.Junction
+import com.example.study.chapter8.code.Chapter811.Road
+import com.example.study.chapter8.code.Chapter811.RoutePlanner1
+import com.example.study.chapter8.code.Chapter811.RoutePlanner2
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+class Chapter811Test(
+    private val routePlanner1: RoutePlanner1 = RoutePlanner1(),
+    private val routePlanner2: RoutePlanner2 = RoutePlanner2(),
+) : DescribeSpec({
+    val startPoint = "xx"
+    val endPoint = "xx"
+
+    describe("routePlanner1.planRoute 메소드는") {
+        context("startPoint와 endPoint가 주어졌을 때") {
+            it("서울의 도로 리스트와 IC 리스트를 리턴한다.") {
+                val result = routePlanner1.planRoute(startPoint, endPoint)
+
+                result.first.shouldBeInstanceOf<List<Road>>()
+                result.first.shouldBeInstanceOf<List<Junction>>()
+
+                result.first.first().name shouldBe "강변대로"
+                result.second.first().name shouldBe "양재IC"
+            }
+        }
+    }
+
+    describe("routePlanner2.planRoute 메소드는") {
+        context("startPoint와 endPoint가 주어졌을 때") {
+            it("부산의 도로 리스트와 IC 리스트를 리턴한다.") {
+                val result = routePlanner2.planRoute(startPoint, endPoint)
+
+                result.first.shouldBeInstanceOf<List<Road>>()
+                result.first.shouldBeInstanceOf<List<Junction>>()
+
+                result.first.first().name shouldBe "부산대로"
+                result.second.first().name shouldBe "부산IC"
+            }
+        }
+    }
+})
