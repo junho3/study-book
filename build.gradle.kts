b589e3a7f4f682fa248f03720da438db7d479066
junho3 <chojunho433@gmail.com>
2023-07-05 20:47:03 +0900

    lint 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
-    
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
 }
